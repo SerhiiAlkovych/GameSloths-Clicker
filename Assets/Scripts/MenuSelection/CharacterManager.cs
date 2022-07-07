bbc68f9b33477d9f2b73e20fb0cbde635cb60e3f
Serhii Alkovych <serhii.alkovych@outlook.com>
2022-07-07 13:47:55 +0200

    Add a player selection scene with small DB and save option

---
+++ Assets/Scripts/MenuSelection/CharacterManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour
+{
+    public CharacterDatabase _characterDB;
+    public Image artworkSprite;
+
+    public Text _characterNameText;
+    public Text _characterGoldText;
+    public Text _characterAttackText;
+
+    private int selectedOption = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("selectedOption")) {
+            selectedOption = 0;
+        }
+        else
+        {
+            Load();
+        }
+        UpdateCharacter(selectedOption);
+    }
+
+    public void NextOption()
+    {
+        selectedOption ++;
+
+        if(selectedOption >= _characterDB.CharacterCount) 
+        {
+            selectedOption = 0;
+        }
+        UpdateCharacter(selectedOption);
+        Save();
+    }
+
+    public void BackOption()
+    {
+        selectedOption --;
+
+        if(selectedOption < 0) 
+        {
+            selectedOption = _characterDB.CharacterCount -1;
+        }
+        UpdateCharacter(selectedOption);
+        Save();
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        Character character = _characterDB.GetCharacter(selectedOption);
+        artworkSprite.sprite = character._characterSprite;
+        _characterNameText.text = character._characterName;
+        _characterGoldText.text = character._characterGold;
+        _characterAttackText.text = character._characterAttack;
+    }
+
+    private void Load()
+    {
+        selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("selectedOption", selectedOption);
+    }
+}
